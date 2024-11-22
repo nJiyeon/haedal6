cc22fc9e54639b29340574a19e197bf21f0bf43b
clcok <ekfrha24@gmail.com>
2024-11-23 00:48:15 +0900

    feat: setting service

--- src/main/java/com/server/photospot/hackathon/service/PlaceService.java
+++ src/main/java/com/server/photospot/hackathon/service/PlaceService.java
@@ -1,5 +1,8 @@
 package com.server.photospot.hackathon.service;
 
+import com.server.photospot.hackathon.dto.req.PlacesRequest;
+import com.server.photospot.hackathon.dto.res.PlaceDetailResponse;
+import com.server.photospot.hackathon.dto.res.PlacesResponse;
 import com.server.photospot.hackathon.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,13 @@ import org.springframework.stereotype.Service;
 public class PlaceService {
     private final PlaceRepository placeRepository;
 
+    public PlacesResponse findPlaceByFourSeasons(PlacesRequest request){
+        PlacesResponse response = null;
+        return response;
+    }
 
+    public PlaceDetailResponse placeDetail(Float latitude, Float longitude){
+        PlaceDetailResponse response = null;
+        return response;
+    }
 }
