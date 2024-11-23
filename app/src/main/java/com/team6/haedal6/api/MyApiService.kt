c2955fff0a48bb5aea8c358aa533bbd168469a06
nJiyeon <chxistinn@gmail.com>
2024-11-23 15:23:58 +0900

    Add Api Modules to communicate

---
+++ app/src/main/java/com/team6/haedal6/api/MyApiService.kt
@@ -0,0 +1,22 @@
+package com.team6.haedal6.api
+
+import com.team6.haedal6.model.Coordinate
+import com.team6.haedal6.model.Place
+import com.team6.haedal6.model.Seasons
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface MyApiService {
+    @POST("/api/getPlaces")
+    suspend fun getPlaces(
+        @Body targetSeasons: Seasons
+        ):Response<Coordinate>
+    @GET("/api/getPlaceDetail")
+    suspend fun getPlaceDetailInfo(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double
+    ): Response<Place>
+}
---
+++ app/src/main/java/com/team6/haedal6/di/MyServiceModule.kt
@@ -0,0 +1,33 @@
+package com.team6.haedal6.di
+
+import com.team6.haedal6.api.MyApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+import javax.inject.Qualifier
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MyServiceModule {
+
+    @Provides
+    @Singleton
+    @OtherRetrofit
+    fun provideOtherRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://127.0.0.1:8080")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providedService(@OtherRetrofit retrofit: Retrofit): MyApiService =
+        retrofit.create(MyApiService::class.java)
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class OtherRetrofit
